536e209c910391ac3643fe9f5ba4497787e7874c
simone-nolberto <simone.nolberto@outlook.com>
2024-03-21 15:00:02 +0100

    update with all components creation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script>
+import AppHeader from '/src/components/AppHeader.vue';
+import AppMain from '/src/components/AppMain.vue';
+import AppFooter from '/src/components/AppFooter.vue';
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'Hello World!'
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
 
 </script>
 
 <template>
- 
+
+
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
+
 </template>
 
-<style>
-
-</style>
+<style></style>
