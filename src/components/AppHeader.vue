c0039e6e6b84b828adc9230b780f9d8d0de43db2
simone-nolberto <simone.nolberto@outlook.com>
2024-03-21 15:00:28 +0100

    update with header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  name: 'AppHeader',
+}
+
+</script>
+
+<template>
+    <header>
+        <img src="" alt="">
+
+        <nav>
+            <a href="">Home</a>
+            <a href="">Prodotti</a>
+            <a href="">Chi siamo</a>
+            <a href="">Contatti</a>
+
+        </nav>
+    </header>
+
+  
+</template>
+
+<style></style>
